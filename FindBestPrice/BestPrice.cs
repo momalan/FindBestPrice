404cd4539c6ad6487a5c2c4d2c278fc757e700c7
Muhammed Malanovic <muhammed.malanovic9@gmail.com>
2019-11-20 22:21:12 +0100

    Importing Project

---
+++ FindBestPrice/BestPrice.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindBestPrice
+{
+    public class BestPrice
+    {
+        public int FindBestPrice(int[] prices, int[] costs)
+        {
+
+            var pricesCosts = new List<Tuple<int, int>>();
+            for (int i = 0; i <= prices.Count() - 1; i++)
+            {
+                pricesCosts.Add(new Tuple<int, int>(prices[i], costs[i]));
+            }
+            pricesCosts = pricesCosts.OrderBy(i => i.Item1).ThenBy(t => t.Item2).ToList();
+            var pricesProfits = new List<Tuple<int, int>>();
+            for (int i = 0; i <= prices.Count() - 1; i++)
+            {
+                int profitOfThisPrice = pricesCosts[i].Item1 - pricesCosts[i].Item2;
+                int profitOfNextPrice = 0;
+                for (int j = i + 1; j < prices.Count(); j++)
+                {
+                    if (pricesCosts[i].Item1 > pricesCosts[j].Item2)
+                    {
+                        profitOfNextPrice = pricesCosts[i].Item1 - pricesCosts[j].Item2;
+                        profitOfThisPrice = profitOfThisPrice + profitOfNextPrice;
+                    }
+                }
+
+                pricesProfits.Add(new Tuple<int, int>(pricesCosts[i].Item1, profitOfThisPrice));
+
+            }
+
+
+            List<int> sameProfitPrices = new List<int>();
+            pricesProfits = pricesProfits.OrderBy(i => i.Item2).ToList();
+            int sameProfitPrice = pricesProfits[prices.Count() - 1].Item2;
+            for (int i = 0; i <= prices.Count() - 2; i++)
+            {
+                if (pricesProfits[i].Item2 == sameProfitPrice)
+                {
+                    sameProfitPrices.Add(pricesProfits[i].Item1);
+                }
+            }
+            if (pricesProfits[prices.Count() - 1].Item2 < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                if (sameProfitPrices == null)
+                {
+                    int bestPrice = pricesProfits[pricesProfits.Count() - 1].Item1;
+                    return bestPrice;
+                }
+                else
+                {
+                    sameProfitPrices.Add(pricesProfits[prices.Count() - 1].Item1);
+                    sameProfitPrices.Sort();
+                    return sameProfitPrices[0];
+                }
+
+            }
+        }
+    }
+}
+
---
+++ FindBestPrice/Controllers/HomeController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindBestPrice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddPrice(string price, string cost, string PriceList, string CostList)
+        {
+            if (price == "" && cost == "")
+            {
+                List<int> pricess = System.Web.Helpers.Json.Decode<List<int>>(PriceList);
+                List<int> costss = System.Web.Helpers.Json.Decode<List<int>>(CostList);
+                int[] prices = pricess.ToArray();
+                int[] costs = costss.ToArray();
+                BestPrice bp = new BestPrice();
+                int bestPrice = bp.FindBestPrice(prices, costs);
+                string stringPricesx = string.Join(", ", prices);
+                string stringPrices = "{" + stringPricesx + "}";
+                string stringCostsx = string.Join(", ", costs);
+                string stringCosts = "{" + stringCostsx + "}";
+                ViewBag.stringPricesx = stringPrices;
+                ViewBag.stringCostsx = stringCosts;
+                ViewBag.BestPrice = bestPrice;
+                return View("BestPrice");
+            }
+            else
+            {
+                List<int> pricesCount = System.Web.Helpers.Json.Decode<List<int>>(PriceList);
+                int priceCounter;
+                if (pricesCount == null)
+                {
+                    priceCounter = 0;
+                }
+                else
+                {
+                    priceCounter = pricesCount.Count();
+                }
+
+                if (Int32.Parse(price) < 1 || Int32.Parse(cost) < 0 || priceCounter > 49)
+                {
+                    if (priceCounter != 0)
+                    {
+                        List<int> pricess = System.Web.Helpers.Json.Decode<List<int>>(PriceList);
+                        List<int> costss = System.Web.Helpers.Json.Decode<List<int>>(CostList);
+                        int[] prices = pricess.ToArray();
+                        int[] costs = costss.ToArray();
+                        ViewBag.PriceList = pricess;
+                        ViewBag.CostList = costss;
+                        string stringPricesx = string.Join(", ", prices);
+                        string stringPrices = "{" + stringPricesx + "}";
+                        string stringCostsx = string.Join(", ", costs);
+                        string stringCosts = "{" + stringCostsx + "}";
+                        ViewBag.stringPrices = stringPrices;
+                        ViewBag.stringCosts = stringCosts;
+                        ViewBag.errorMessage = "Error: Price should be more than 1, cost should not be < 0 OR there is no space for more comparison (max 50)!";
+                        return View("Index");
+                    }
+                    else
+                    {
+                        ViewBag.errorMessage = "Error: Price should be more than 1, cost should not be < 0 OR there is no space for more Prices!";
+                        return View("Index");
+                    }
+                }
+                else
+                {
+                    int priceX = Int32.Parse(price);
+                    int costX = Int32.Parse(cost);
+                    List<int> prices = System.Web.Helpers.Json.Decode<List<int>>(PriceList);
+                    List<int> costs = System.Web.Helpers.Json.Decode<List<int>>(CostList);
+                    if (prices != null && costs != null)
+                    {
+                        prices.Add(priceX);
+                        costs.Add(costX);
+                        ViewBag.PriceList = prices;
+                        ViewBag.CostList = costs;
+                        string stringPricesx = string.Join(", ", prices);
+                        string stringPrices = "{" + stringPricesx + "}";
+                        string stringCostsx = string.Join(", ", costs);
+                        string stringCosts = "{" + stringCostsx + "}";
+                        ViewBag.stringPrices = stringPrices;
+                        ViewBag.stringCosts = stringCosts;
+                    }
+                    else
+                    {
+                        List<int> pricess = new List<int>();
+                        List<int> costss = new List<int>();
+                        pricess.Add(priceX);
+                        costss.Add(costX);
+                        ViewBag.PriceList = pricess;
+                        ViewBag.CostList = costss;
+                        string stringPricesx = string.Join(", ", pricess);
+                        string stringPrices = "{" + stringPricesx + "}";
+                        string stringCostsx = string.Join(", ", costss);
+                        string stringCosts = "{" + stringCostsx + "}";
+                        ViewBag.stringPrices = stringPrices;
+                        ViewBag.stringCosts = stringCosts;
+                    }
+                    return View("Index");
+                }
+            }
+        }
+    }
+}
